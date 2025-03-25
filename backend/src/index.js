5c79def3463a647eff62607c380a1d6044235d19
Tushar Gupta <kamalgupta11341@gmail.com>
2025-03-25 16:00:49 +0530

    Signup page complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,17 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials : true,
+}))
 
 const PORT = process.env.port || 5001;
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true, // Required for WSL2 file system
+      interval: 1000, // Poll every second
+    },
+    hmr: {
+      clientPort: 5173, // Explicit HMR port
+    },
+  },
 });
