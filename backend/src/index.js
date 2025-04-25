59c2a4a26cbdb8de380cba12dbfd88a7ecc1483f
Tushar Gupta <kamalgupta11341@gmail.com>
2025-04-25 15:53:52 +0530

    package updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use(
 const PORT = process.env.port || 5001;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
